55084ffa21f02ba849cd9bbf7b1e13b45d9b258f
Bernd Verhofstadt <bernd@verhofstadt.eu>
2014-11-23 00:35:24 +0100

    Added script to upload pictures to this server.
    This script will check the extension, check if name is unique and save
    it on the server.

--- 1_source/4_webserver/picture_management/upload.php
+++ 1_source/4_webserver/picture_management/upload.php
@@ -1,5 +1,41 @@
-<?pho
-// UPLOAD IMAGES
+<?php
+//UPLOAD IMAGES	
 
+try {
+    $allow = array("jpg", "jpeg", "gif", "png");
+	$todir = 'user_images/';		
+	if (!!$_FILES['file']['tmp_name'] ) // is the file uploaded yet?
+	{
+	$info = explode('.', strtolower( $_FILES['file']['name']) ); // whats the extension of the file
+	if ( in_array( end($info), $allow) ) // is this file allowed
+	{
+		$new_file_name = $todir . $this->user_id . '.' . end($info);
+		if ( move_uploaded_file( $_FILES['file']['tmp_name'], $new_file_name) )
+		{
+			$file_location = '/' . $new_file_name;
+			$this->foto = $file_location;
+		}
+	}
+	else
+	{
+		throw new Exception('');		// error this file extension is not allowed, default avatar is set.
+		//echo "false";
+	}
+}
+else
+{
+	throw new Exception('Failed to upload!');
+	//echo "Failed to upload!";
+}
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+if($_POST)
+{
 
+}
+else
+{
+echo "send something...";
+}
 ?>
