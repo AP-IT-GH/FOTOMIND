32417b836d9d331e7a633b1af00cddd4d7c74481
Frederik Moonen <fre.moonen@gmail.com>
2014-11-24 11:43:15 +0100

    Fixed connection to camera
    We now have a connection from the raspberrypi to the camera & can upload
    the photo's to the server.

--- 1_source/1_raspberry-pi/GetFileFromDirectoryAndSendToServer.sh
+++ 1_source/1_raspberry-pi/GetFileFromDirectoryAndSendToServer.sh
@@ -4,11 +4,11 @@ if [ $ACTION == download ];then
 dir='/mnt/ramdisk' #directory where we store our photo's temporary
 file=$(/bin/ls -1 "$dir" | head -1) #list one file (there can only be one file processed at a time
 path=$(readlink --canonicalize "$dir/$file") #make a path to the file
-server='http://www.verhofstadt.eu/upload.php'
+server='http://192.168.1.121:8888/fotomind/upload.php'
 mac=$(ifconfig eth0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
 #use this line for uploading, hope it works if bernd has the site online
 #curl -F "userid=1" -F "filecomment=This is an image file" -F "image=@/home/user/desktop/img.jpg" localhost/upload.php
-curl -F "mac=$mac" -F "image=$path" $server
+curl -F "mac=$mac" -F "image=@$path" $server
 rm $path #remove file when it is send, this will remove nothing if there is no file
 else
         exit 0
--- 1_source/1_raspberry-pi/GphotoStartupScript.sh
+++ 1_source/1_raspberry-pi/GphotoStartupScript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #saved at /home/pi
-(cd /mnt/ramdisk && /usr/local/bin/gphoto2 --capture-tethered --hook-script /home/pi/GetFileFromDirectoryAndSendToServer.sh)
-#/usr/local/bin/gphoto2 --capture-tethered --hook-script /home/pi/GetFileFromDirectoryAndSendToServer.sh -filename /mnt/ramdisk/picture%n.jpg
+cd /mnt/ramdisk # && /usr/local/bin/gphoto2 --capture-tethered --hook-script /home/pi/GetFileFromDirectoryAndSendToServer.sh)
+/usr/local/bin/gphoto2 --capture-tethered --hook-script /home/pi/GetFileFromDirectoryAndSendToServer.sh
