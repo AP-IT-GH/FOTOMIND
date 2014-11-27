59f1f747af90c570e53fd5669fdeacdbaa2d0e4f
Robin Vercammen <robinvercammen1993@hotmail.com>
2014-11-27 16:23:05 +0100

    Arduino Code For Testing
    Send from computer to arduino master, take picture from sended command

--- 1_source/0_arduino/v2.0/WirelessTriggerTest/WirelessTriggerTest.ino
+++ 1_source/0_arduino/v2.0/WirelessTriggerTest/WirelessTriggerTest.ino
@@ -1,4 +1,70 @@
+#include <SPI.h>
+#include <CommunicationMedium.h>
+#include <nRF24L01.h>
+#include <RF24_config.h>
+#include <RF24.h>
+#include <ArduinoInteraktiv.h>
+#include <DistanceSRF04.h>
+
+#define SENDER
+//#define RECEIVER
+
+CommunicationMedium Master;
+
+String UltraSonicKey = "117";//u
+String TriggerManualKey = "105";//i
+String NoTriggerKey = "112";//p
+String LatestReceive = "";
+
+DistanceSRF04 Dist;
+int distance;
+
+ArduinoInteraktiv camera; 
+
 void setup()
-{}
+{
+  camera.Start(); 
+  Serial.begin(57600);
+  Master.init();  
+  Dist.begin(4,5); //(echo, trigger)
+}
 void loop()
-{}
+{
+  #ifdef SENDER
+    if(Serial.available()>0)
+    {
+      byte incomingByte = Serial.read();
+      Master.SendData((String)incomingByte);      
+    }
+  #endif
+  #ifdef RECEIVER
+    String receipt = Master.RecieveData();
+    if(receipt!="empty")
+    {
+      LatestReceive = receipt;             
+    }
+    //if loops
+    if(receipt == UltraSonicKey)
+      triggerUltrasonic();
+    else if(receipt == TriggerManualKey)
+      triggerManual();
+    else if(receipt == NoTriggerKey)
+      noMoreTriggers(); 
+  #endif
+}
+void triggerUltrasonic()
+{
+  distance = Dist.getDistanceCentimeter();
+  if (distance <= 10)
+  {
+    camera.SinglePicture();
+  }
+}
+void triggerManual()
+{
+  camera.SinglePicture();
+}
+void noMoreTriggers()
+{
+  
+}
--- 2_hardware/2_prototypes/README.md
+++
@@ -1,4 +0,0 @@
-# Prototypes
-
-Plaats in deze folder enkel informatie en documentatie i.v.m. hardware prototypes die voor het project ontwikkeld is.
-
